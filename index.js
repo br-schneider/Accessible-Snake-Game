000aefe77b14d997333679b8e578444f6cd74ade
Brett Schneider <brett@Bretts-MBP.lan1>
2020-12-02 17:08:34 -0600

    Deleted node server

--- index.js
+++
@@ -1,9 +0,0 @@
-dom.window.gameLoaded = () => {
-  let port = process.env.PORT;
-  if (port == null || port == "") {
-    port = 8082;
-  }
-  server.listen(port, function () {
-    console.log(`Listening on ${server.address().port}`);
-  });
-};
